b5f43efabe48e0b0fa853dc3e4f77cca1d8f2f65
qiuxinchao <qiuxinchao@fbank.com.cn>
2022-01-25 11:39:23 +0800

    向测试环境发送kafka

--- src/main/java/com/yichen/basic/BasicApplication.java
+++ src/main/java/com/yichen/basic/BasicApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.yichen.*")
 @EnableSwagger2
 public class BasicApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BasicApplication.class, args);
     }
