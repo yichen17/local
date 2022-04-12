b764298d6ab38435f8a94d2fa46c0efb345b85bd
qiuxinchao <qiuxinchao@fbank.com.cn>
2022-04-12 08:51:03 +0800

    拦截器和过滤器实现，使得@Valid校验有效

---
+++ src/main/java/com/yichen/basic/config/DecryptConfiguration.java
@@ -0,0 +1,41 @@
+package com.yichen.basic.config;
+
+import com.yichen.basic.filter.DecryptFilter;
+import com.yichen.basic.interceptor.DecryptInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Qiuxinchao
+ * @version 1.0
+ * @date 2022/3/30 21:46
+ * @describe 解密配置
+ *     参考解决办法  https://yaoyuanyy.github.io/2019/09/21/springboot%20HandlerIntercepter%E6%8B%A6%E6%88%AA%E5%99%A8%E5%AE%9E%E7%8E%B0%E4%BF%AE%E6%94%B9request%20body%E6%95%B0%E6%8D%AE/
+ */
+@Configuration
+public class DecryptConfiguration implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        DecryptInterceptor interceptor = new DecryptInterceptor();
+        registry.addInterceptor(interceptor);
+    }
+
+    @Bean
+    public FilterRegistrationBean<?> decryptDataRegistrationBean(){
+        DecryptFilter filter = new DecryptFilter();
+        FilterRegistrationBean<DecryptFilter> bean = new FilterRegistrationBean<>();
+        bean.setName("decrypt-data");
+        bean.setFilter(filter);
+        bean.addUrlPatterns("/*");
+        bean.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return bean;
+    }
+
+
+}
